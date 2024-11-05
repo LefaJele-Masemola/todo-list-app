9134ec9ba444f4edd19f7be214167b19d93f118f
Lefa <136033739+LefaJele-Masemola@users.noreply.github.com>
2024-11-05 20:40:53 +0200

    created routes that responds to a CRUD operation

--- src/routes/operations.js
+++ src/routes/operations.js
@@ -1,1 +1,10 @@
-troll
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+router.post('/', taskController.addTask);
+router.get('/', taskController.getTasks);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router;
